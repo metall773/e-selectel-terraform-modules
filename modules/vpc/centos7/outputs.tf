5ff21591fcdaec879e8e19ea052b76387c399dde
Evgeniy Sevostyanov <Evgeniy.Sevostyanov@tipico.com>
2020-11-28 22:42:55 +0100

    net 22

--- modules/vpc/centos7/outputs.tf
+++ modules/vpc/centos7/outputs.tf
@@ -5,7 +5,3 @@ output "server_id" {
 output "server_port_id" {
   value = openstack_networking_port_v2.port_1.id
 }
-
-output "floating_ip" {
-  value = data.openstack_networking_floatingip_v2.floating_ip
-}
--- modules/vpc/centos8/data.tf
+++
@@ -1,3 +0,0 @@
-data "openstack_networking_floatingip_v2" "floating_ip" {
-  address = module.floatingip.floatingip_address
-}
