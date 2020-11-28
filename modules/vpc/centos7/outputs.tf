1b231f5adbf06f223ba46dc81c159628d2bf296c
Evgeniy Sevostyanov <Evgeniy.Sevostyanov@tipico.com>
2020-11-28 23:32:04 +0100

    net 33

--- modules/vpc/centos7/outputs.tf
+++ modules/vpc/centos7/outputs.tf
@@ -5,3 +5,7 @@ output "server_id" {
 output "server_port_id" {
   value = openstack_networking_port_v2.port_1.id
 }
+
+output "server_FQDN" {
+  value = "${var.server_name}.${var.vm_dns_domain_name}"
+}
