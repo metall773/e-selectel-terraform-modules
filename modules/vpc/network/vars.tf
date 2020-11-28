a2ae372bf201e46d880ac3ba8761d73ac8b36ab2
Evgeniy Sevostyanov <Evgeniy.Sevostyanov@tipico.com>
2020-11-28 20:31:27 +0100

    net 4

---
+++ modules/vpc/network/vars.tf
@@ -0,0 +1,15 @@
+variable "router_external_net_name" {
+  default = "external_network"
+}
+
+variable "router_name" {
+  default = "router"
+}
+
+variable "network_name" {
+  default = "network"
+}
+
+variable "subnet_cidr" {
+  default = "192.168.70.0/24"
+}
