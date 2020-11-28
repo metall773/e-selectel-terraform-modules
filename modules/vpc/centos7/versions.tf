a13f45f553849c2c12f57de7fb6d3c9b41cc3b4c
Evgeniy Sevostyanov <Evgeniy.Sevostyanov@tipico.com>
2020-11-29 00:42:25 +0100

    net 35

--- modules/vpc/centos7/versions.tf
+++ modules/vpc/centos7/versions.tf
@@ -3,8 +3,8 @@ terraform {
     openstack = {
       source = "terraform-provider-openstack/openstack"
     }
-    random = {
-      source = "hashicorp/random"
+    selectel = {
+      source = "selectel/selectel"
     }
   }
   required_version = ">= 0.13"
