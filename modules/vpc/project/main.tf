3fe2d698f35a1584a279a0c0d5a0b3125fd31de3
Evgeniy S <lee040404@gmail.com>
2020-12-07 19:00:40 +0100

    dynmic fix2

--- modules/vpc/project/main.tf
+++ modules/vpc/project/main.tf
@@ -5,11 +5,11 @@ resource "selectel_vpc_project_v2" "project_1" {
   dynamic "quotas" {
     for_each = var.quotas
     content {
-      resource_name = each.key
+      resource_name = quotas.key
       resource_quotas {
         region = var.os_region
         zone   = var.server_zone
-        value  = each.value
+        value  = quotas.value
       }
     }
   }
