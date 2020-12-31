6911cd88627eff91b5acbcd3763ffb742bb583cd
Evgeniy S <lee040404@gmail.com>
2020-12-31 19:54:06 +0100

    test2

--- modules/vpc/windows/main.tf
+++ modules/vpc/windows/main.tf
@@ -95,7 +95,7 @@ resource "selectel_domains_record_v1" "a_record_local" {
   domain_id = var.vm_dns_domain_id
   name      = "local.${var.server_name}.${var.vm_dns_domain_name}"
   type      = "A"
-  content   = openstack_compute_instance_v2.instance_1.network.[1].fixed_ip_v4
+  content   = openstack_compute_instance_v2.instance_1.network[1].fixed_ip_v4
   ttl       = 60
 }
 
