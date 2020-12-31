eb2c060af25f4678ecdad17c5dca2e97020d446d
Evgeniy S <lee040404@gmail.com>
2020-12-31 20:42:07 +0100

    test9

--- modules/vpc/windows/first-boot.ps1
+++ modules/vpc/windows/first-boot.ps1
@@ -1,21 +1,6 @@
 #ps1_sysnative
-$logFile = 'c:\init-log.txt'
-Function LogWrite
-{
-  Param ([string]$log1, [string]$log2, [string]$log3, [string]$log4,  [string]$log5)
-  $stamp = (Get-Date).toString("yyyy/MM/dd HH:mm:ss")
-  $line = "$stamp $log1 $log2 $log3 $log4 $log5"
-  Write-host $line
-  Add-content $logFile -value $Line
-}
-LogWrite "------------------------------------------------"
-LogWrite "Script start"
-LogWrite "Script file: " $PSScriptRoot
-LogWrite "------------------------------------------------"
-LogWrite "Set TimeZone Russia TZ 2 Standard Time"
-Set-TimeZone -Name "Russia TZ 2 Standard Time"
+test9Set-TimeZone -Name "Russia TZ 2 Standard Time"
 $choco_list="${install_packages}"
-#install choco packages
 if ( $choco_list -ne "" ) {
     LogWrite "Install choco packages:"
     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
@@ -54,4 +39,8 @@ Get-Content "$env:temp\e-keys\*.pub" | Set-Content "C:\Users\Administrator\.ssh\
 Remove-Item –path "$env:temp\e-keys" -Force -Recurse
 $acl = Get-Acl "C:\Users\$ssh_user\.ssh\authorized_keys"
 $acl.SetAccessRuleProtection($true, $false)
+$administratorsRule = New-Object system.security.accesscontrol.filesystemaccessrule("Administrators","FullControl","Allow")
+$systemRule = New-Object system.security.accesscontrol.filesystemaccessrule("SYSTEM","FullControl","Allow")
+$acl.SetAccessRule($administratorsRule)
+$acl.SetAccessRule($systemRule)
 $acl | Set-Acl
