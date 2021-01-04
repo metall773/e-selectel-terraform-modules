b824cd5e4c3035e261dd59105b0d41924743b108
Evgeniy S <lee040404@gmail.com>
2021-01-04 20:50:16 +0100

    test12

--- modules/vpc/windows/main.tf
+++ modules/vpc/windows/main.tf
@@ -41,9 +41,8 @@ resource "openstack_compute_instance_v2" "instance_1" {
     boot_index       = 0
   }
 
-
+  for_each         = openstack_blockstorage_volume_v3.volumes
   block_device {
-    for_each         = openstack_blockstorage_volume_v3.volumes
     uuid             = openstack_blockstorage_volume_v3.volumes[each.key].id
     source_type      = "volume"
     destination_type = "volume"
