13f8025a6fcd4d23830caa73a4785aedb34ea4b8
Evgeniy S <lee040404@gmail.com>
2021-01-04 22:22:33 +0100

    enable_online_resize

--- modules/vpc/windows/volumes.tf
+++ modules/vpc/windows/volumes.tf
@@ -4,10 +4,11 @@ module "image_datasource" {
 }
 
 resource "openstack_blockstorage_volume_v3" "volumes" {
-  for_each          = var.data_volumes
-  availability_zone = var.server_zone
-  description       = "Volume ${each.key} for server ${var.server_name}"
-  name              = "${var.server_name}_${each.key}"
-  size              = each.value.size_gb
-  volume_type       = each.value.volume_type
+  for_each             = var.data_volumes
+  availability_zone    = var.server_zone
+  description          = "Volume ${each.key} for server ${var.server_name}"
+  name                 = "${var.server_name}_${each.key}"
+  size                 = each.value.size_gb
+  volume_type          = each.value.volume_type
+  enable_online_resize = true
 }
